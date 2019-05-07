5336174b196ed342685a07067855b9f8f8181bce
PauloNobre <paulinho140@gmail.com>
2019-05-07 08:46:17 -0300

    43: Pipes (usando pipes, parâmetros e pipes aninhados)

---
+++ src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.css']
+})
+export class ExemplosPipesComponent implements OnInit {
+
+  livro: any= {
+    titulo: 'A Guerra dos Tronos. As Crônicas de Gelo e Fogo - Livro 1',
+    rating: 4.6548,
+    numeroPaginas: 592,
+    preco: 63.12,
+    dataLancamento: new Date(2015, 8, 15),
+    url: 'https://www.amazon.com.br/Guerra-Tronos-Cr%C3%B4nicas-Gelo-Fogo/dp/8544102921'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
